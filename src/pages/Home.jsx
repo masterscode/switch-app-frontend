6199e8798f5ed096408af8a25496b301d0934302
masterscode <ogbinakaemmanuel@gmail.com>
2021-07-17 18:53:34 +0100

    in progress

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,15 @@
 import React from "react";
 import { NavBar } from "../components/NavBar";
+import aedc from "../assets/images/service-providers/aedc.svg";
+import eedc from "../assets/images/service-providers/eedc.svg";
+import ekedc from "../assets/images/service-providers/ekedc.svg";
+import ibedc from "../assets/images/service-providers/ibedc.svg";
+import ikeja from "../assets/images/service-providers/ikeja.svg";
+import jedc from "../assets/images/service-providers/jedc.svg";
+import kaduna from "../assets/images/service-providers/kaduna.svg";
+import kedco from "../assets/images/service-providers/kedco.svg";
+import phedc from "../assets/images/service-providers/kedco.svg";
+
 
 export const Home = () => {
   return (
@@ -13,14 +23,14 @@ export const Home = () => {
           </h2>
 
           <h5 className="mt-5 mb-0">Enter your number here to begin</h5>
-          <div class="form-floating mb-1 mt-1 text-black-50 fw-bold">
+          <div className="form-floating mb-1 mt-1 text-black-50 fw-bold">
             <input
               type="tel"
               className="form-control"
               id="phone-number"
               placeholder="000 0000 000"
             />
-            <label for="phone-number">Phone Number</label>
+            <label htmlFor="phone-number">Phone Number</label>
           </div>
           <button className="btn btn-lg w-100 btn-primary p-3 text-center">
             Buy Electricity
@@ -29,8 +39,15 @@ export const Home = () => {
         </div>
       </section>
     
-        <section className='home__service-providers bg-secondary p-1 rounded'>
-
+        <section className='home__service-providers bg-light p-4  rounded d-flex' >
+            <img src={aedc} alt="service provider aedc" className="img-thumbnail rounded " />
+            <img src={eedc} alt="service provider eedc" className="img-thumbnail rounded " />
+            <img src={ekedc} alt="service provider ekedc" className="img-thumbnail rounded " />
+            <img src={ibedc} alt="service provider ibedc" className="img-thumbnail rounded " />
+            <img src={ikeja} alt="service provider ikeja" className="img-thumbnail rounded " />
+            <img src={kaduna} alt="service provider kaduna" className="img-thumbnail rounded " />
+            <img src={jedc} alt="service provider jedc" className="img-thumbnail rounded " />
+            <img src={kedco} alt="service provider kedco" className="img-thumbnail rounded " />
         </section>
        </main>
   );
