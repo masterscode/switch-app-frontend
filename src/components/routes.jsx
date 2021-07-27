4bd2ceb29831922e868efc48437ec0234f74c5ae
masterscode <ogbinakaemmanuel@gmail.com>
2021-07-27 13:26:11 +0100

    b4 pull

--- src/components/routes.jsx
+++ src/components/routes.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { Switch, Route } from 'react-router';
 import Dashboard from '../pages/dashboard';
 import { Home } from '../pages/Home';
+import { Registration } from '../pages/Registration';
 
 const Routes = ()=>{
     return (
         <Switch>
-            <Route to='/' exact component={Home} />
-            <Route to='/dashboard' component={Dashboard} />
+            <Route path='/' exact component={Home} />
+            <Route path='/dashboard' component={Dashboard} />
+            <Route path='/register' exact  component={Registration} />
         </Switch>
     );
 }
