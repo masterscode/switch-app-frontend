ba584b310af128518c3443e50725bd689d585bb6
Pat4lyf <itotojesupat@gmail.com>
2021-07-31 22:19:33 +0100

    ...

--- src/components/routes.jsx
+++ src/components/routes.jsx
@@ -3,6 +3,7 @@ import { Switch, Route } from "react-router";
 import Dashboard from "../pages/dashboard";
 import { Home } from "../pages/Home";
 import { Login } from "../pages/Login";
+import { Register } from "../pages/Register";
 
 const Routes = () => {
   return (
@@ -10,6 +11,7 @@ const Routes = () => {
       <Route path="/" exact component={Home} />
       <Route path="/dashboard" exact component={Dashboard} />
       <Route path="/login" exact component={Login} />
+      <Route path="/register" exact component={Register} />
     </Switch>
   );
 };
