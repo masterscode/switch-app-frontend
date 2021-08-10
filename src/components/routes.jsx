7ece7ad018d9b1b8dce4b6967e3e8fd2f70921ab
masterscode <ogbinakaemmanuel@gmail.com>
2021-08-10 14:57:00 +0100

    add linting

--- src/components/routes.jsx
+++ src/components/routes.jsx
@@ -1,19 +1,17 @@
-import React from "react";
-import { Switch, Route } from "react-router";
-import Dashboard from "../pages/dashboard";
-import { Home } from "../pages/Home";
-import { Login } from "../pages/Login";
-import { Register } from "../pages/Register";
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import Dashboard from '../pages/dashboard';
+import { Home } from '../pages/Home';
+import { Login } from '../pages/Login';
+import Register from '../pages/Register';
 
-const Routes = () => {
-  return (
-    <Switch>
-      <Route path="/" exact component={Home} />
-      <Route path="/dashboard" exact component={Dashboard} />
-      <Route path="/login" exact component={Login} />
-      <Route path="/register" exact component={Register} />
-    </Switch>
-  );
-};
+const Routes = () => (
+  <Switch>
+    <Route path="/" exact component={Home} />
+    <Route path="/dashboard" exact component={Dashboard} />
+    <Route path="/login" exact component={Login} />
+    <Route path="/register" exact component={Register} />
+  </Switch>
+);
 
 export default Routes;
